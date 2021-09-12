03d170a28dfe90ef59753d5e23972bc712f6e1d2
Atstaka <81695610+Atstaka@users.noreply.github.com>
2021-09-12 21:52:02 +0300

    Add files via upload

---
+++ My First Password Generator.py
@@ -0,0 +1,6 @@
+import random
+import string
+numbers = 1,2,3,4,5,6,7,8,9,0
+words = string.ascii_letters + string.punctuation + ''.join(str(v)for v in numbers) 
+print("".join(random.choice(words) for i in range(16)))
+
